220d09bdf72d7c2a96bdc1513324734b813f164d
G00403720 <G00403720@atu.ie>
2022-11-21 10:12:26 +0000

    myWriter

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,10 +1,22 @@
+package ie.atu.Lab8;
 
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Storage {
     public static void main(String[] args) {
-        File myFile = new File("MyFile.txt");
-        System.out.println("My File is located at " + myFile.getAbsolutePath());
+        File myFile = new File("MyFile1.txt");
+        System.out.println("My file is located at " + myFile.getAbsolutePath());
+
+        try {
+            FileWriter myWriter = new FileWriter("MyWriter.txt");
+            myWriter.write("This is my second line");
+            myWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
